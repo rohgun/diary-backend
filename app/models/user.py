10e1a9b7d5c3a1662d090ac5b045d96f3afdbe8c
Minsuh Roh <127650200+rohgun@users.noreply.github.com>
2025-10-29 16:05:58 +0900

    change

--- app/models/user.py
+++ app/models/user.py
@@ -6,7 +6,7 @@ from bson import ObjectId
 # 전역 설정
 # ====================================================
 # bcrypt → passlib context로 관리
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 
 # ====================================================
