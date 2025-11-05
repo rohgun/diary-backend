75a7b53e8a85eaf56628a311988e77c5c7fa70d9
Minsuh Roh <127650200+rohgun@users.noreply.github.com>
2025-11-05 16:13:43 +0900

    list error  fix

--- app/schemas/diary.py
+++ app/schemas/diary.py
@@ -1,7 +1,7 @@
 # app/schemas/diary.py
 from pydantic import BaseModel, Field
 from datetime import datetime
-from typing import Optional
+from typing import Optional, List
 
 # ==================================================
 # ✅ 감정 구조 정의
